617be32701dfcd4626bf588a14d694ffde400ff1
Gianluca Fiore <forod.g@gmail.com>
2018-04-18 20:10:04 +0200

    removed simpleWeather, reset js

--- Intermediate_Frontend_Development_Projects/Local_Weather_App/js/weather.js
+++ Intermediate_Frontend_Development_Projects/Local_Weather_App/js/weather.js
@@ -1,28 +1,10 @@
 /* Check if browser supports geolocation */
 if ("geolocation" in navigator) {
-	$('.js-geolocation').show();
+	navigator.geolocation.getCurrentPosition(showPosition);
 } else {
-	$('.js-geolocation').hide();
+	$("#weather").html("<p>Geolocation is not supported by this browser, sorry</p>");
 }
 
-function loadWeather(location, woeid) {
-	$.simpleWeather({
-		location: location,
-		woeid: woeid,
-		unit: 'c',
-		success: function(weather) {
-			html =
-				'<p' + weather.temp + '&deg;' + weather.units.temp + '</p>';
-
-			$("#weather").html(html);
-		},
-		error: function(error) {
-			$("#weather").html("<p>An error occurred</p>");
-		}
-	});
+function showPosition(position) {
+	$("#weather").html(position.coords.latitude + " " + position.coords.longitude);
 };
-
-
-$(document).ready(function() {
-	loadWeather('Krakow', '');
-});
