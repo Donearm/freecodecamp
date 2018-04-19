5273fc239b15f6fa08e0bab0d31447490e99c504
Gianluca Fiore <forod.g@gmail.com>
2018-04-19 20:52:19 +0200

    implemented Freecodecamp Weather API with fetch

--- Intermediate_Frontend_Development_Projects/Local_Weather_App/js/weather.js
+++ Intermediate_Frontend_Development_Projects/Local_Weather_App/js/weather.js
@@ -5,6 +5,17 @@ if ("geolocation" in navigator) {
 	$("#weather").html("<p>Geolocation is not supported by this browser, sorry</p>");
 }
 
+async function fetchWeather(lati, long) {
+	const URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lati}&lon=${long}`;
+	const fetchResult = fetch(URL);
+	const response = await fetchResult;
+	const jsonData = await response.json();
+	return jsonData;
+}
+
 function showPosition(position) {
+	let weather = fetchWeather(Math.floor(position.coords.latitude), Math.floor(position.coords.longitude));
+	console.log(weather);
+
 	$("#weather").html(position.coords.latitude + " " + position.coords.longitude);
 };
