8da34f2918879d89e6156e32ec234bab2bd3b7d1
Gianluca Fiore <forod.g@gmail.com>
2018-04-22 15:23:29 +0200

    using a button to switch C/F

--- Intermediate_Frontend_Development_Projects/Local_Weather_App/js/weather.js
+++ Intermediate_Frontend_Development_Projects/Local_Weather_App/js/weather.js
@@ -33,15 +33,14 @@ function showPosition(position) {
 			let tempF = celsiusToF(tempC);
 			let toggleT = false;
 			$("#weather").html("<h2>In</h2> <h1>" + json.name + "</h1> <h2>we have</h2> <h1>" + json.weather[0].description + "</h1>");
-			$("#temp").children("h2").text("Temperature is " + tempC);
+			$("#temp").children("h2").text("Temperature is " + tempC + "°C");
+			$("#temp").children("button").text("C/F");
 			$("#tempswitch").on('click', function() {
 				toggleT = !toggleT;
 				if (toggleT) {
-					$("#temp").children("h2").text("Temperature is " + tempF);
-					$("#temp").children("a").text("°F");
+					$("#temp").children("h2").text("Temperature is " + tempF + "°F");
 				} else {
-					$("#temp").children("h2").text("Temperature is " + tempC);
-					$("#temp").children("a").text("°C");
+					$("#temp").children("h2").text("Temperature is " + tempC + "°C");
 				}
 			});
 			$("#weathericon").html("<img src=\"" + json.weather[0].icon + "\">");
