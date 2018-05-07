66a16cbb453497c534b88a9ae0f9a1c95523ff56
Gianluca Fiore <forod.g@gmail.com>
2018-05-07 21:03:54 +0200

    add a separate ajax request for a random article

--- Intermediate_Frontend_Development_Projects/Wikipedia_Viewer/js/wikipedia.js
+++ Intermediate_Frontend_Development_Projects/Wikipedia_Viewer/js/wikipedia.js
@@ -1,6 +1,43 @@
 
 $(document).ready(function() {
 
+	$("#random").click(function(event) {
+		// Set up random Wikipedia page url
+		const randomUrl = "https://en.wikipedia.org/wiki/Special:Random";
+		$.ajax({
+			type: "GET",
+			url: randomUrl,
+			contentType: "application/json; charset=utf-8",
+			async: false,
+			dataType: "json",
+			data: {
+				origin: "*"
+			},
+			success: function (data, textStatus, jqXHR) {
+				let wikibody = data.parse.text["*"];
+				let wikititle = data.parse.title;
+				let blurb = $('<div></div>').html(wikibody);
+				console.log(url);
+
+				// Point all href attributes to english Wikipedia's 
+				// corresponding pages
+				blurb.find("a").each(function() {
+					let newUrl = $(this).attr('href').replace('/wiki', 'https://en.wikipedia.org/wiki');
+					$(this).attr('href', newUrl);
+				});
+
+				// remove occasional cite errors
+				blurb.find('.mw-ext-cite-error').remove();
+				// remove any references
+				blurb.find('sup').remove();
+
+				$("#articletitle").html(wikititle);
+				$("#article").html(blurb);
+				console.log(data);
+			},
+		});
+	});
+
 	$("#submitbutton").click(function(event) {
 		let search = $("input").serializeArray();
 		console.log(search[0].value);
