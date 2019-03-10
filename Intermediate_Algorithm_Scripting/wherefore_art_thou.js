85b50fe75163334d151e940f673bdd6d9da8035f
Gianluca Fiore <forod.g@gmail.com>
2019-03-10 14:57:58 +0100

    add description to Wherefore Art Thou exercise

--- Intermediate_Algorithm_Scripting/wherefore_art_thou.js
+++ Intermediate_Algorithm_Scripting/wherefore_art_thou.js
@@ -1,3 +1,5 @@
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
 function whatIsInAName(collection, source) {
   // Only change code below this line
   let k = Object.keys(source);
